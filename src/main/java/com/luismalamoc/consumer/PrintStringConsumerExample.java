30b7fdcde105a669d37bdb28b49c86ee45c1992e
Luis Mauricio Alamo <luismalamoc@gmail.com>
2022-05-17 15:24:05 -0400

    feat: consumer and producer examples for apache kafka

---
+++ src/main/java/com/luismalamoc/consumer/PrintStringConsumerExample.java
@@ -0,0 +1,41 @@
+package com.luismalamoc.consumer;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Properties;
+
+public class PrintStringConsumerExample {
+
+    public static void main(String[] args) {
+
+        //consumer properties
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "localhost:9092");
+        props.put("group.id", "test-group");
+
+        //using auto commit
+        props.put("enable.auto.commit", "true");
+
+        //string inputs and outputs
+        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+
+        //kafka consumer object
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(props);
+
+        //subscribe to topic
+        consumer.subscribe(Arrays.asList("mauricio-topic"));
+
+        //infinite poll loop
+        while (true) {
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofSeconds(1));
+            for (ConsumerRecord<String, String> record : records)
+                System.out.printf("offset = %d, key = %s, value = %s\n", record.offset(), record.key(), record.value());
+        }
+
+    }
+}
